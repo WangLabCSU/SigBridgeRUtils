b3f691c52f10fe42ee2f98d9ff38dfabaaa1152d
Exceret <15364051195@163.com>
2026-01-16 22:13:52 -0500

    py configuration

--- R/21_crate.R
+++ R/21_crate.R
@@ -20,18 +20,18 @@
 #' @seealso [carrier::crate()]
 #' @export
 crate <- function(
-    .fn,
-    ...,
-    .parent_env = baseenv(),
-    .error_arg = ".fn",
-    .error_call = environment()
+  .fn,
+  ...,
+  .parent_env = baseenv(),
+  .error_arg = ".fn",
+  .error_call = environment()
 ) {
-    rlang::check_installed("carrier")
-    getExportedValue("carrier", "crate")(
-        .fn = .fn,
-        ...,
-        .parent_env = .parent_env,
-        .error_arg = .error_arg,
-        .error_call = .error_call
-    )
+  rlang::check_installed("carrier")
+  getExportedValue("carrier", "crate")(
+    .fn = .fn,
+    ...,
+    .parent_env = .parent_env,
+    .error_arg = .error_arg,
+    .error_call = .error_call
+  )
 }
